6f997869d409b12b5672694ecf615f4037dd82c8
CoderAnchel <alex21fd@gmail.com>
2022-10-13 18:09:06 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,200 @@
+const todo = document.querySelector(".todo");
+
+const btn = document.querySelector(".btn");
+const btn500 = document.querySelector(".btn500");
+const btn600 = document.querySelector(".btn600");
+const btn700 = document.querySelector(".btn700");
+const btn800 = document.querySelector(".btn700");
+
+const sor = document.querySelector(".sorpresa");
+const sor2 = document.querySelector(".sorpresa2");
+const sor3 = document.querySelector(".sorpresa3");
+
+const des1 = document.querySelector(".desactivar");
+const des2 = document.querySelector(".desactivar2");
+const des3 = document.querySelector(".desactivar3");
+
+const des4 = document.querySelector(".desactivar4");
+
+
+btn.addEventListener("click", () => {
+    sor.classList.toggle("active")
+    des1.classList.toggle("act")
+})
+
+btn500.addEventListener("click", () => {
+    des1.classList.toggle("off")
+    des2.classList.toggle("act")
+})
+
+btn600.addEventListener("click", () => {
+    des2.classList.toggle("off")
+    des3.classList.toggle("act")
+})
+
+
+btn700.addEventListener("click", () => {
+    des3.classList.toggle("off")
+    des4.classList.toggle("act")
+})
+
+btn800.addEventListener("click", () => {
+    des4.classList.toggle("off")
+    todo.classList.toggle("active");
+})
+
+
+const btn1 = document.querySelector(".btn1");
+const btn200 = document.querySelector(".btn200");
+const btn300 = document.querySelector(".btn300");
+const btn4 = document.querySelector(".btn4");
+const btn5 = document.querySelector(".btn5");
+const btn6 = document.querySelector(".btn6");
+const btn7 = document.querySelector(".btn7");
+const btn8 = document.querySelector(".btn8");
+const btn9 = document.querySelector(".btn9");
+const btn10 = document.querySelector(".btn10");
+const btn11 = document.querySelector(".btn11");
+const btn12 = document.querySelector(".btn12");
+const btn13 = document.querySelector(".btn13");
+const btn14 = document.querySelector(".btn14");
+const btn15 = document.querySelector(".btn15");
+const btn16 = document.querySelector(".btn16");
+const btn17 = document.querySelector(".btn17");
+const btn18 = document.querySelector(".btn18");
+const btn19 = document.querySelector(".btn19");
+const btn20 = document.querySelector(".btn20");
+const btn21 = document.querySelector(".btn21");
+const btn22 = document.querySelector(".btn22");
+const btn23 = document.querySelector(".btn23");
+const btn100 = document.querySelector(".btn23");
+
+const letra2 = document.querySelector(".letra2");
+const letra3 = document.querySelector(".letra3");
+const letra4 = document.querySelector(".letra4");
+const letra5 = document.querySelector(".letra5");
+const letra6 = document.querySelector(".letra6");
+const letra7 = document.querySelector(".letra7");
+const letra8 = document.querySelector(".letra8");
+const letra9 = document.querySelector(".letra9");
+const letra10 = document.querySelector(".letra10");
+const letra11 = document.querySelector(".letra11");
+const letra12 = document.querySelector(".letra12");
+const letra13 = document.querySelector(".letra13");
+const letra14 = document.querySelector(".letra14");
+const letra15 = document.querySelector(".letra15");
+const letra16 = document.querySelector(".letra16");
+const letra17 = document.querySelector(".letra17");
+const letra18 = document.querySelector(".letra18");
+const letra19 = document.querySelector(".letra19");
+const letra20= document.querySelector(".letra20");
+const letra21= document.querySelector(".letra21");
+const letra22= document.querySelector(".letra22");
+const letraa= document.querySelector(".letraa");
+
+
+
+
+btn1.addEventListener("click", () => {
+    letra2.classList.toggle("active")
+})
+
+
+btn200.addEventListener("click", () => {
+    letra3.classList.toggle("active")
+})
+
+btn300.addEventListener("click", () => {
+    letra4.classList.toggle("active")
+})
+
+btn4.addEventListener("click", () => {
+    letra5.classList.toggle("active")
+})
+
+
+btn6.addEventListener("click", () => {
+    letra6.classList.toggle("active")
+})
+
+
+btn7.addEventListener("click", () => {
+    letra7.classList.toggle("active")
+})
+
+
+btn8.addEventListener("click", () => {
+    letra8.classList.toggle("active")
+})
+
+btn9.addEventListener("click", () => {
+    letra9.classList.toggle("active")
+})
+
+btn10.addEventListener("click", () => {
+    letra10.classList.toggle("active")
+})
+
+btn11.addEventListener("click", () => {
+    letra11.classList.toggle("active")
+})
+
+btn12.addEventListener("click", () => {
+    letra12.classList.toggle("active")
+})
+
+btn13.addEventListener("click", () => {
+    letra13.classList.toggle("active")
+})
+
+btn14.addEventListener("click", () => {
+    letra14.classList.toggle("active")
+})
+
+btn15.addEventListener("click", () => {
+    letra15.classList.toggle("active")
+})
+
+btn16.addEventListener("click", () => {
+    letra16.classList.toggle("active")
+})
+
+btn17.addEventListener("click", () => {
+    letra17.classList.toggle("active")
+})
+
+btn18.addEventListener("click", () => {
+    letra18.classList.toggle("active")
+})
+
+btn19.addEventListener("click", () => {
+    letra19.classList.toggle("active")
+})
+
+btn20.addEventListener("click", () => {
+    letra20.classList.toggle("active")
+})
+
+
+btn21.addEventListener("click", () => {
+    letra21.classList.toggle("active")
+})
+
+btn22.addEventListener("click", () => {
+    letra22.classList.toggle("active")
+})
+
+const tapa = document.querySelector(".tapa");
+const card = document.querySelector(".card1");
+
+
+tapa.addEventListener("click", () => {
+    card.classList.toggle("act");
+})
+
+const tapa2 = document.querySelector(".tapa2");
+const card2 = document.querySelector(".card2");
+
+tapa2.addEventListener("click", () => {
+    card2.classList.toggle("act");
+})
